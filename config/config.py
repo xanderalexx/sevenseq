93541074228dc1fc60144cf00cc9b252eeb8d9f5
Alexander Morales <lx16hd@gmail.com>
2021-11-23 10:05:55 +0000

    added config.py, 7seq.py modified to work with config...

---
+++ config/config.py
@@ -0,0 +1,33 @@
+import RPi.GPIO as g
+
+# for pinmode, enter pin layout mode
+#
+# for GPIO numbering, choose g.BCM  
+#
+# or, for pin numbering, choose g.BOARD 
+
+pinmode = g.BCM
+
+# enter corresponding pin numbers associated with each
+# individual led on each digit
+# ex: led1a is led A on the 1st digit
+# ex: led2f is Led F on the 2nd digit
+# image of layout here: https://www.engineersgarage.com/wp-content/uploads/2019/10/7-segment-display-with-8051-microcontroller_orig-1.png
+
+led1a = 22
+led1b = 27
+led1c = 18
+led1d = 17
+led1e = 4
+led1f = 23
+led1g = 24
+led1dot = 15
+
+led2a = 3
+led2b = 2
+led2c = 14
+led2d = 10
+led2e = 9
+led2f = 11
+led2g = 0
+led2dot = 5
